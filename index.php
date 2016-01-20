939d704f4d89947602b93825b8980befce6db1b0
Riney <fails.to.install@gmail.com>
2016-01-19 20:33:42 -0800

    cleanup

--- index.php
+++ index.php
@@ -12,19 +12,16 @@
 
 </head>
 <body class="container">
-
     <h1>Totally Lookup A Movie</h1>
-    
-     <form method="GET" action="" class="form">
+    <form method="GET" action="" class="form">
         <input type="search" 
                 name="title" 
                 class="form-control" 
                 value= "<?= htmlspecialchars($_GET['title'], ENT_QUOTES, 'Utf-8') ?>"
                 placeholder="search by title">
     </form>
-    
-     <table>
-         <thead>
+    <table>
+        <thead>
             <tr>
                 <th>Title</th>
                 <th class="text-right">Release Date</th>
@@ -33,41 +30,30 @@
             </tr>
         </thead>
         <tbody>
+            
       <?php
-      
-      
-
-      
-      
-require_once 'connection.php';
-require_once 'models/movie-model.php';
-$con = getConnection();
-$moviemodel = new MeowSearch($con);
-
-
-        //$q = $_GET['q'];
-        //$matches = $moviemodel->getDetail($q);
-
-        $q = $_GET['title'];
-        $matches = $moviemodel->search($q);
-    
-    //print_r($matches);
-    foreach($matches as $row):
-    ?>
-    <tr>
-        <td><a href="views/detail.php?q=<?= $row['ID']?>"><?= $row['title']?></td>
-        <td><?= $row['released']?></td>
-        <td><?= $row['tickets']?></td>
-        <td>$<?= $row['gross']?></td>
-    </tr>
-     <?php 
-    endforeach;
-    ?>
-      
-        </tbody>
-    </table>
-    
-   
-</body>
-</html>
+      require_once 'connection.php';
+      require_once 'models/movie-model.php';
+      $con = getConnection();
+      $moviemodel = new MeowSearch($con);
+      //$q = $_GET['q'];
+      //$matches = $moviemodel->getDetail($q);
+      $q = $_GET['title'];
+      $matches = $moviemodel->search($q);
+      //print_r($matches);
+      foreach($matches as $row):
+      ?>
+      <tr>
+          <td><a href="views/detail.php?q=<?= $row['ID']?>"><?= $row['title']?></td>
+          <td><?= $row['released']?></td>
+          <td><?= $row['tickets']?></td>
+          <td>$<?= $row['gross']?></td>
+      </tr>
+      <?php 
+      endforeach;
+      ?>
+      </tbody>
+      </table>
+      </body>
+      </html>
 
