e4972b6798334da97e2b27bc6a1dbfd0e292df07
Riney <fails.to.install@gmail.com>
2016-01-19 20:43:39 -0800

    cleanup

--- models/movie-model.php
+++ models/movie-model.php
@@ -1,17 +1,22 @@
 <?php 
 class MeowSearch {
     protected $conn;
+    
     public function __construct($conn) {
         $this->conn = $conn;
     }
     
     public function search($q) {
+        
         $sql = 'select ID,title,released,gross,tickets from moviesTable2014';
         if($q){
             $sql = "select title,released,gross,tickets from moviesTable2014 where title like '%{$q}%'";
         }
-        //$stmt = $this->conn->prepare($sql);
+        
+       // $stmt = $this->conn->prepare($sql);
         //$success = $stmt->execute(array($q,$q));
+
+        
         $stmt = $this->conn->query($sql);
         if (!$stmt) {
            var_dump($stmt->errorInfo());
